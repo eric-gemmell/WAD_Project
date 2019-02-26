dfab81327fc9961a4bdbd45eb8ae3cdd91f283d2
palamalama <ericgemmell@gmail.com>
2019-02-26 20:20:05 +0000

    started up django

---
+++ Oh_My_Nom/Oh_My_Nom/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Oh_My_Nom project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Oh_My_Nom.settings')
+
+application = get_wsgi_application()
