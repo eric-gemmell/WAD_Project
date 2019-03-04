e97e46cf4dba58261ae36718e94d3c0a72211ba2
palamalama <ericgemmell@gmail.com>
2019-03-04 18:31:48 +0000

    templates served

--- Oh_My_Nom/main/tests.py
+++ Oh_My_Nom/main/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.staticfiles import finders
-
+from django import template
 
 class GeneralTests(TestCase):
 	def setUp(self):
@@ -14,5 +14,9 @@ class GeneralTests(TestCase):
 		response = self.client.get(reverse('main:index'))
 		if(response.content == b""):
 			self.fail("index page contains no content!")
-
-
+	
+	def test_templates_are_served(self):
+		try:
+			template.loader.get_template("main/index.html")	
+		except:
+			self.fail("could not find index template, are you sure templates are being served?")
