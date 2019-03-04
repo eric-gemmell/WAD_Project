9556cee4701fc15eaf3996270f1c48c5759e5f78
palamalama <ericgemmell@gmail.com>
2019-03-04 18:02:46 +0000

    tests are now operational!

--- Oh_My_Nom/main/tests.py
+++ Oh_My_Nom/main/tests.py
@@ -1,5 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.staticfiles import finders
 
-class AnimalTestCase(TestCase):
-	def StartupWorks(self):
-		print("Working")
+
+class GeneralTests(TestCase):
+	def setUp(self):
+		pass
+	
+	def tearDown(self):
+		pass
+	
+	def test_index_page_exists(self):
+		response = self.client.get(reverse('index'))
+		if(response.content == b""):
+			self.fail("index page contains no content!")
+
+
