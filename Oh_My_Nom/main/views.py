31df78e4413ed24161994805574aedc267c81431
palamalama <ericgemmell@gmail.com>
2019-03-08 18:45:19 +0000

    login register page working NO TESTS

--- Oh_My_Nom/main/views.py
+++ Oh_My_Nom/main/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from main.forms import UserForm, UserProfileForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 
@@ -19,23 +18,65 @@ def randomrecipes(request):
 
 def registersignin(request):
 	registered = False
+	context_dict={}
 	if request.method == 'POST':
-		user_form = UserForm(data=request.POST)
-		profile_form = UserProfileForm(data=request.POST)
-		if user_form.is_valid() and profile_form.is_valid():
-			user = user_form.save()
-			user.set_password(user.password)
-			user.save()
-			profile = profile_form.save(commit=False)
-			profile.user = user
-			registered = True
-		else:
-			print(user_form.errors, profile_form.errors)
-	else:
-		user_form = UserForm()
-		profile_form = UserProfileForm()
+		if(request.POST.get("registerusername") and request.POST.get("registerpassword")):
+			print("\n\n GOT REGISTER INFORMATION \n\n")
+			username = request.POST.get("registerusername")
+			password = request.POST.get("registerpassword")
+			try:
+				User.objects.get(username=username)
+				print("\n\nNON UNIQUE USERNAME")
+				context_dict["register_error"] = "An account with that username already exists"
+			except:
+				print("\n\nUNIQUE USERNAME")
+				user = User(username=username)
+				user.save()
+				#STILL need to check if password meets minum requirements
+				#CHECK IF PASSWORD IS GOOD HERE
+				user.set_password(password)
+				user.save()
+				print("\n\n Successfully saved user\n\n")
+				user = authenticate(username=username, password=password)
+				if user:
+					 if user.is_active:
+                                		login(request, user)
+                                		return HttpResponseRedirect(reverse('main:index'))
 
-	return render(request,"main/registersignin.html",{'user_form': user_form,'profile_form': profile_form,'registered': registered})
+		elif(request.POST.get("signinusername") and request.POST.get("signinpassword")):
+			print("\n\n GOT SIGNIN INFORMATION \n\n")
+			username = request.POST.get('signinusername')
+			password = request.POST.get('signinpassword')
+			user = authenticate(username=username, password=password)
+			if user:
+				if user.is_active:
+					login(request, user)
+					return HttpResponseRedirect(reverse('main:index'))
+				else:
+					context_dict["signin_error"] = "Your account is disabled"
+			else:
+				print("Invalid login details: {0}, {1}".format(username, password))
+				context_dict["signin_error"] = "Incorrect login details"
+		
+	return render(request,"main/registersignin.html",context = context_dict)
+
+def signin(request):
+	print("received request in sign in !")
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(username=username, password=password)
+		if user:
+			if user.is_active:
+				login(request, user)
+				return HttpResponseRedirect(reverse('main:index'))
+			else:	
+				return HttpResponse("Your account is disabled.")
+		else:
+			print("Invalid login details: {0}, {1}".format(username, password))
+			return HttpResponse("Invalid login details supplied.")
+	else:
+		return HttpResponseRedirect(reverse("main:index"))
 
 def tempsignin(request):
 	if request.method == 'POST':
@@ -54,10 +95,16 @@ def tempsignin(request):
 	else:
 		return render(request, 'main/tempsignin.html', {})
 
+def tempsignout(request):
+	logout(request)
+	return HttpResponseRedirect(reverse("main:index"))
+
 def myplaces(request):
 	return render(request,"main/myplaces.html")
 
 def myrecipes(request):
+	if not request.user.is_authenticated:
+		return HttpResponseRedirect(reverse("main:tempsignin"))
 	return render(request,"main/myrecipes.html")
 
 def test(request):
