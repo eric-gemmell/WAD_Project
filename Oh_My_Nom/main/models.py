2fba4ae96dbae97693be697fbd4e34caf1fb5019
palamalama <ericgemmell@gmail.com>
2019-03-17 15:10:58 +0000

    somehow userinfo working

--- Oh_My_Nom/main/models.py
+++ Oh_My_Nom/main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=128)
